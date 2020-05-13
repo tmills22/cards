761cade2d330b9b1e0012246a5cb29c316ae3ec3
Travis Mills <tmils22@vms.edu>
2020-05-13 17:30:09 -0600

    1

--- src/App.java
+++ src/App.java
@@ -2,8 +2,16 @@ import processing.core.PApplet;
 
 public class App extends PApplet
 {
+
+    Deck myDeck = new Deck();
+
     public static void main(String[] args)
     {
         PApplet.main("App");
+    }
+
+    public void settings()
+    {
+        System.out.println(myDeck);
     }
 }
